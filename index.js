955993746af6b111137fed50cda7d6c1d6c8108a
YadBro <ncada708@gmail.com>
2022-07-05 23:47:51 +0700

    add Procfile

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const router    = require("./src/routes");
 
 const app   = express();
 app.use(express.json());
-const PORT  = 5000;
+const PORT  = process.env.PORT || 5000;
 
 
 app.use("/api/v1", router);
 
+app.get("/", (req, res) => {
+    res.send("HELLO");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
